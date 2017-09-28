b079d8f1e9416a56e841ee99b81cd3ffffb87f07
kaytemori <kayte@ualberta.ca>
2017-09-28 09:35:10 -0600

    Edits to MySineWave.py

--- MySineWave.py
+++ MySineWave.py
@@ -5,10 +5,10 @@ Created on Thu Sep 14 07:58:33 2017
 
 @author: kaytemori
 """
+import matplotlib.pylab as plt
+import numpy as np
 
 def MySineWave(n):
-    import matplotlib.pylab as plt
-    import numpy as np
     x=np.linspace(-np.pi*2, np.pi*2, 200)
     plt.plot(x, np.sin((np.pi*2*x)/n))
     plt.show()
